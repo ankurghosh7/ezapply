35dccb21ca494a62f03fd9d4652e8538c343bb80
ankur ghosh <ankurghosh041@gmail.com>
2024-04-08 19:22:13 +0530

    add user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,29 +1,29 @@
 import mongoose, { Document, Schema } from "mongoose";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+// import bcrypt from "bcrypt";
+// import jwt from "jsonwebtoken";
 
-interface UserDocument extends Document {
+interface UserDocument {
   first_name: string;
   last_name: string;
   email: string;
   verifyEmail: boolean;
   phone_number: string;
   verifyPhone: boolean;
-  exprience: string;
-  type: string;
+  work_exprience: string;
   password: string;
   avatar: string;
   files: Map<string, string>;
   city: string;
   country: string;
   refershToken: string;
-  comparePassword(password: string): Promise<boolean>;
-  generatRefershToken: () => string;
-  generatAccessToken: () => string;
 }
 const userSchema = new Schema<UserDocument>(
   {
-    fullName: {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
       type: String,
       required: true,
     },
@@ -38,7 +38,7 @@ const userSchema = new Schema<UserDocument>(
       type: Boolean,
       default: false,
     },
-    phone: {
+    phone_number: {
       type: String,
       required: true,
       unique: true,
@@ -47,13 +47,9 @@ const userSchema = new Schema<UserDocument>(
       type: Boolean,
       default: false,
     },
-    exprience: {
+    work_exprience: {
       type: String,
       required: true,
-    },
-    type: {
-      type: String,
-      default: "jobseeker",
     },
     password: {
       type: String,
@@ -80,38 +76,38 @@ const userSchema = new Schema<UserDocument>(
   { timestamps: true }
 );
 
-userSchema.pre("save", async function (next: (error?: Error) => void) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-});
+// userSchema.pre("save", async function (next: (error?: Error) => void) {
+//   if (!this.isModified("password")) return next();
+//   this.password = await bcrypt.hash(this.password, 10);
+// });
 
-userSchema.methods.comparePassword = async function (password: string) {
-  return await bcrypt.compare(password, this.password);
-};
-userSchema.methods.generatAccessToken = function () {
-  return jwt.sign(
-    {
-      id: this._id,
-      email: this.email,
-      name: this.name,
-      phone: this.phone,
-    },
-    process.env.ACCESS_TOKEN_SECRET!,
-    {
-      expiresIn: process.env.ACCESS_TOKEN_TIME,
-    }
-  );
-};
-userSchema.methods.generatRefershToken = function () {
-  return jwt.sign(
-    {
-      id: this._id,
-    },
-    process.env.REFRESH_TOKEN_SECRET!,
-    {
-      expiresIn: process.env.REFRESH_TOKEN_TIME,
-    }
-  );
-};
+// userSchema.methods.comparePassword = async function (password: string) {
+//   return await bcrypt.compare(password, this.password);
+// };
+// userSchema.methods.generatAccessToken = function () {
+//   return jwt.sign(
+//     {
+//       id: this._id,
+//       email: this.email,
+//       name: this.name,
+//       phone: this.phone,
+//     },
+//     process.env.ACCESS_TOKEN_SECRET!,
+//     {
+//       expiresIn: process.env.ACCESS_TOKEN_TIME,
+//     }
+//   );
+// };
+// userSchema.methods.generatRefershToken = function () {
+//   return jwt.sign(
+//     {
+//       id: this._id,
+//     },
+//     process.env.REFRESH_TOKEN_SECRET!,
+//     {
+//       expiresIn: process.env.REFRESH_TOKEN_TIME,
+//     }
+//   );
+// };
 export const User =
   mongoose.models.users || mongoose.model("users", userSchema);
